2899631822fb8747f2624de54e4dd263ea87866c
HebiKotei <c1yvan@outlook.com>
2023-02-26 17:51:06 +0000

    Revert "Refactor(server): Extracted Function"
    This reverts commit abf49f6db0967bd19e3348c4ef7bbc77d36eb8c0.

--- server.lua
+++ server.lua
@@ -1,39 +1,11 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
----@param item string The item that is required by the recipe
----@param requirement integer The amount required by the recipe
----@return boolean callback The value sent back to the client
-local function loadIngredients(item, requirement)
-    local src = source
-	local Player = QBCore.Functions.GetPlayer(src)
-    local itemCount = exports.ox_inventory:GetItem(src, item, nil, true)
-	if not Player.PlayerData.items then
-        TriggerClientEvent('ox_lib:notify', source, {type = 'error', description = Lang:t("error.no_items")})
-        return false
-    end
-    if itemCount < requirement then
-        TriggerClientEvent('ox_lib:notify', source, {type = 'error', description = Lang:t("error.invalid_items")})
-        return false
-    end
-    Player.Functions.RemoveItem(item, requirement, false)
-    return true
-end
-
-lib.callback.register('qb-vineyard:server:loadIngredients', function()
-    return loadIngredients('grapejuice', 23)
-end)
-
-lib.callback.register('qb-vineyard:server:grapeJuice', function()
-    return loadIngredients('grape', 16)
-end)
-
 RegisterNetEvent('qb-vineyard:server:getGrapes', function()
     local Player = QBCore.Functions.GetPlayer(source)
     local amount = math.random(Config.GrapeAmount.min, Config.GrapeAmount.max)
     Player.Functions.AddItem("grape", amount)
 end)
 
-<<<<<<< HEAD
 lib.callback.register('qb-vineyard:server:loadIngredients', function(source)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
@@ -68,20 +40,12 @@ end)
 
 RegisterNetEvent('qb-vineyard:server:receiveWine', function()
     local Player = QBCore.Functions.GetPlayer(tonumber(source))
-=======
-RegisterNetEvent('qb-vineyard:server:receiveWine', function()
-	local Player = QBCore.Functions.GetPlayer(source)
->>>>>>> b783c68 (Refactor(server): Extracted Function)
     local amount = math.random(Config.WineAmount.min, Config.WineAmount.max)
     Player.Functions.AddItem("wine", amount, false)
 end)
 
 RegisterNetEvent('qb-vineyard:server:receiveGrapeJuice', function()
-<<<<<<< HEAD
     local Player = QBCore.Functions.GetPlayer(tonumber(source))
-=======
-	local Player = QBCore.Functions.GetPlayer(source)
->>>>>>> b783c68 (Refactor(server): Extracted Function)
     local amount = math.random(Config.GrapeJuiceAmount.min, Config.GrapeJuiceAmount.max)
     Player.Functions.AddItem("grapejuice", amount, false)
 end)
