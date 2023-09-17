bc05479f89f7f0146759b77a8e783b672b860ad8
Matthew <mattmitch714@gmail.com>
2023-09-17 14:13:40 -0400

    fix: forgot import.lua (#10)

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,6 +5,7 @@ description 'https://github.com/Qbox-project/qbx-vineyard'
 version '1.1.0'
 
 shared_scripts {
+    '@qbx-core/import.lua',
     '@ox_lib/init.lua',
     '@qb-core/shared/locale.lua',
     'locales/en.lua',
