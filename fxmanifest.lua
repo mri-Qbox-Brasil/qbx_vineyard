0bc3f9c3c694ddeb34bb803ccd1f167d6eb82679
Matthew <22198949+MafewTM@users.noreply.github.com>
2023-11-16 13:51:14 -0500

    fix: oal and remove import.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,12 +1,11 @@
 fx_version 'cerulean'
 game 'gta5'
 
-description 'QBX-Vineyard'
+description 'QBX_Vineyard'
 repository 'https://github.com/Qbox-project/qbx_vineyard'
 version '1.1.0'
 
 shared_scripts {
-    '@qbx_core/import.lua',
     '@ox_lib/init.lua',
     '@qbx_core/shared/locale.lua',
     'locales/en.lua',
@@ -22,3 +21,4 @@ client_scripts {
 }
 
 lua54 'yes'
+use_experimental_fxv2_oal 'yes'
